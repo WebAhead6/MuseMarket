dbbc782478b1a4f86bf9b655f50c66ff59669664
Abeer Rizik <abeer.rizik93@gmail.com>
2020-08-18 17:37:34 +0300

    adds login page and signin page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const model = require("../model");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-// routes.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  req.body.name, req.body.password;
+
+  res.redirect("/user");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+router.post("/signup", (req, res) => {
+  console.log(req.body);
+  model
+    .createNewUser(req.body)
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((e) => {
+      res.render("signup", { error: e.message });
+    });
+});
 
 module.exports = router;
